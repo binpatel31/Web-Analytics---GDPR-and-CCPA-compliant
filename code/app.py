64c060a426d129f15bf2fb8b809f1914de130f57
binpatel31 <binpatel31@gmail.com>
2020-10-03 17:41:03 +0000

    updated app.py AND added requirements.txt

--- code/app.py
+++ code/app.py
@@ -2,25 +2,28 @@ from flask import Flask, jsonify, abort, make_response, request, render_template
 import json
 import requests
 from elasticsearch import Elasticsearch
+import datetime
 
 app = Flask(__name__)
 
 
 '''
-POST implementation: creation of new todo
+POST implementation: creation of new user record
 '''
 @app.route('/')
-def create_todo_note():
+def home_page():
     return render_template("webpage.html")
+
 
 @app.route("/json_data", methods=["POST","GET"])
 def get_json_data():
     if request.method == 'POST':
         data = request.get_json()
+        data["time"] = datetime.datetime.now().strftime("%Y-%m-%d %H")
         es = Elasticsearch([{'host':'localhost','port':9200}])
         res = es.index(index='my-index-000001', body=data)
     return '', 200
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host="0.0.0.0", debug = True)
